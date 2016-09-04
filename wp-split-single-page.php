85d282610f0f6a779701a7d3de8712ca00588c3b
SECT <info@sectwebstudio.com>
2016-09-05 04:30:01 +0900

    Code cleanup

--- wp-split-single-page.php
+++ wp-split-single-page.php
@@ -8,23 +8,23 @@ Version: 1.2.0
 Author URI: https://www.ilovesect.com/
 */
 
-function single_paginate_links( $args = '' ) {
+function single_paginate_links($args = '') {
 	global $wp_query, $wp_rewrite;
 
 	// Setting up default values based on the current URL.
-	$pagenum_link = html_entity_decode( get_pagenum_link() );
-	$url_parts    = explode( '?', $pagenum_link );
+	$pagenum_link = html_entity_decode(get_pagenum_link());
+	$url_parts    = explode('?', $pagenum_link);
 
 	// Get max pages and current page out of the current query, if available.
-	$total   = isset( $wp_query->max_num_pages ) ? $wp_query->max_num_pages : 1;
-	$current = get_query_var( 'paged' ) ? intval( get_query_var( 'paged' ) ) : 1;
+	$total   = isset($wp_query->max_num_pages) ? $wp_query->max_num_pages : 1;
+	$current = get_query_var('paged') ? intval(get_query_var('paged')) : 1;
 
 	// Append the format placeholder to the base URL.
-	$pagenum_link = trailingslashit( $url_parts[0] ) . '%_%';
+	$pagenum_link = trailingslashit($url_parts[0]) . '%_%';
 
 	// URL base depends on permalink settings.
-	$format  = $wp_rewrite->using_index_permalinks() && ! strpos( $pagenum_link, 'index.php' ) ? 'index.php/' : '';
-	$format .= $wp_rewrite->using_permalinks() ? user_trailingslashit( $wp_rewrite->pagination_base . '/%#%', 'paged' ) : '?paged=%#%';
+	$format  = $wp_rewrite->using_index_permalinks() && ! strpos($pagenum_link, 'index.php') ? 'index.php/' : '';
+	$format .= $wp_rewrite->using_permalinks() ? user_trailingslashit($wp_rewrite->pagination_base . '/%#%', 'paged') : '?paged=%#%';
 
 	$defaults = array(
 		'base' => $pagenum_link, // http://example.com/all_posts.php%_% : %_% is replaced by format (below)
@@ -44,42 +44,42 @@ function single_paginate_links( $args = '' ) {
 		'after_page_number' => ''
 	);
 
-	$args = wp_parse_args( $args, $defaults );
+	$args = wp_parse_args($args, $defaults);
 
-	if ( ! is_array( $args['add_args'] ) ) {
+	if (! is_array($args['add_args'])) {
 		$args['add_args'] = array();
 	}
 
 	// Merge additional query vars found in the original URL into 'add_args' array.
-	if ( isset( $url_parts[1] ) ) {
+	if (isset($url_parts[1])) {
 		// Find the format argument.
-		$format = explode( '?', str_replace( '%_%', $args['format'], $args['base'] ) );
-		$format_query = isset( $format[1] ) ? $format[1] : '';
-		wp_parse_str( $format_query, $format_args );
+		$format = explode('?', str_replace('%_%', $args['format'], $args['base']));
+		$format_query = isset($format[1]) ? $format[1] : '';
+		wp_parse_str($format_query, $format_args);
 
 		// Find the query args of the requested URL.
-		wp_parse_str( $url_parts[1], $url_query_args );
+		wp_parse_str($url_parts[1], $url_query_args);
 
 		// Remove the format argument from the array of query arguments, to avoid overwriting custom format.
-		foreach ( $format_args as $format_arg => $format_arg_value ) {
-			unset( $url_query_args[ $format_arg ] );
+		foreach ($format_args as $format_arg => $format_arg_value) {
+			unset($url_query_args[ $format_arg ]);
 		}
 
-		$args['add_args'] = array_merge( $args['add_args'], urlencode_deep( $url_query_args ) );
+		$args['add_args'] = array_merge($args['add_args'], urlencode_deep($url_query_args));
 	}
 
 	// Who knows what else people pass in $args
 	$total = (int) $args['total'];
-	if ( $total < 2 ) {
+	if ($total < 2) {
 		return;
 	}
 	$current  = (int) $args['current'];
 	$end_size = (int) $args['end_size']; // Out of bounds?  Make it the default.
-	if ( $end_size < 1 ) {
+	if ($end_size < 1) {
 		$end_size = 1;
 	}
 	$mid_size = (int) $args['mid_size'];
-	if ( $mid_size < 0 ) {
+	if ($mid_size < 0) {
 		$mid_size = 2;
 	}
 	$add_args = $args['add_args'];
@@ -87,20 +87,20 @@ function single_paginate_links( $args = '' ) {
 	$page_links = array();
 	$dots = false;
 
-	if ( $args['prev_next'] && $current && 1 < $current ) :
-		$link = str_replace( '%_%', 2 == $current ? '' : $args['format'], $args['base'] );
-		$link = str_replace( '%#%', $current - 1, $link );
+	if ($args['prev_next'] && $current && 1 < $current) :
+		$link = str_replace('%_%', 2 == $current ? '' : $args['format'], $args['base']);
+		$link = str_replace('%#%', $current - 1, $link);
 
 		// add code
 		// For Plugin "Public Post Preview"
 		if(isset($_GET['p']) && isset($_GET['_ppp'])){
-			$link = str_replace( '&paged=1', '', $link );
+			$link = str_replace('&paged=1', '', $link);
 		}else{
-			$link = str_replace( '/1/', '/', $link );
+			$link = str_replace('/1/', '/', $link);
 		}
 
-		if ( $add_args )
-			$link = add_query_arg( $add_args, $link );
+		if ($add_args)
+			$link = add_query_arg($add_args, $link);
 		$link .= $args['add_fragment'];
 
 		/**
@@ -110,53 +110,53 @@ function single_paginate_links( $args = '' ) {
 		 *
 		 * @param string $link The paginated link URL.
 		 */
-		$page_links[] = '<a class="prev page-numbers" href="' . esc_url( apply_filters( 'paginate_links', $link ) ) . '">' . $args['prev_text'] . '</a>';
+		$page_links[] = '<a class="prev page-numbers" href="' . esc_url(apply_filters('paginate_links', $link)) . '">' . $args['prev_text'] . '</a>';
 	endif;
-	for ( $n = 1; $n <= $total; $n++ ) :
-		if ( $n == $current ) :
-			$page_links[] = "<span class='page-numbers current'>" . $args['before_page_number'] . number_format_i18n( $n ) . $args['after_page_number'] . "</span>";
+	for ($n = 1; $n <= $total; $n++) :
+		if ($n == $current) :
+			$page_links[] = "<span class='page-numbers current'>" . $args['before_page_number'] . number_format_i18n($n) . $args['after_page_number'] . "</span>";
 			$dots = true;
 		else :
-			if ( $args['show_all'] || ( $n <= $end_size || ( $current && $n >= $current - $mid_size && $n <= $current + $mid_size ) || $n > $total - $end_size ) ) :
+			if ($args['show_all'] || ($n <= $end_size || ($current && $n >= $current - $mid_size && $n <= $current + $mid_size) || $n > $total - $end_size)) :
 			//	$link = str_replace( '%_%', 1 == $n ? '' : $args['format'], $args['base'] );
 
 				// add code
 				if(1 == $n){
 					// For Plugin "Public Post Preview"
 					if(is_preview()){
-						$link = str_replace( '&paged=%#%', '', $args['base'] );
+						$link = str_replace('&paged=%#%', '', $args['base']);
 					}else{
-						$link = str_replace( '%#%/', '', $args['base'] );
+						$link = str_replace('%#%/', '', $args['base']);
 					}
 				}else{
-					$link = str_replace( '%_%', $args['format'], $args['base'] );
+					$link = str_replace('%_%', $args['format'], $args['base']);
 				}
 
-				$link = str_replace( '%#%', $n, $link );
-				if ( $add_args )
-					$link = add_query_arg( $add_args, $link );
+				$link = str_replace('%#%', $n, $link);
+				if ($add_args)
+					$link = add_query_arg($add_args, $link);
 				$link .= $args['add_fragment'];
 
 				/** This filter is documented in wp-includes/general-template.php */
-				$page_links[] = "<a class='page-numbers' href='" . esc_url( apply_filters( 'paginate_links', $link ) ) . "'>" . $args['before_page_number'] . number_format_i18n( $n ) . $args['after_page_number'] . "</a>";
+				$page_links[] = "<a class='page-numbers' href='" . esc_url(apply_filters('paginate_links', $link)) . "'>" . $args['before_page_number'] . number_format_i18n($n) . $args['after_page_number'] . "</a>";
 				$dots = true;
-			elseif ( $dots && ! $args['show_all'] ) :
-				$page_links[] = '<span class="page-numbers dots">' . __( '&hellip;' ) . '</span>';
+			elseif ($dots && ! $args['show_all']) :
+				$page_links[] = '<span class="page-numbers dots">' . __('&hellip;') . '</span>';
 				$dots = false;
 			endif;
 		endif;
 	endfor;
-	if ( $args['prev_next'] && $current && ( $current < $total || -1 == $total ) ) :
-		$link = str_replace( '%_%', $args['format'], $args['base'] );
-		$link = str_replace( '%#%', $current + 1, $link );
-		if ( $add_args )
-			$link = add_query_arg( $add_args, $link );
+	if ($args['prev_next'] && $current && ($current < $total || -1 == $total)) :
+		$link = str_replace('%_%', $args['format'], $args['base']);
+		$link = str_replace('%#%', $current + 1, $link);
+		if ($add_args)
+			$link = add_query_arg($add_args, $link);
 		$link .= $args['add_fragment'];
 
 		/** This filter is documented in wp-includes/general-template.php */
-		$page_links[] = '<a class="next page-numbers" href="' . esc_url( apply_filters( 'paginate_links', $link ) ) . '">' . $args['next_text'] . '</a>';
+		$page_links[] = '<a class="next page-numbers" href="' . esc_url(apply_filters('paginate_links', $link)) . '">' . $args['next_text'] . '</a>';
 	endif;
-	switch ( $args['type'] ) {
+	switch ($args['type']) {
 		case 'array' :
 			return $page_links;
 
@@ -176,7 +176,7 @@ function single_paginate_links( $args = '' ) {
 /**
  * pagination
  */
-function single_paginate( $args = '' ) {
+function single_paginate($args = '') {
 	if(!is_preview()){
 		$args1 = array(
 			'base'           => get_the_permalink() . '%#%/',
@@ -296,7 +296,7 @@ function next_single_paged_link($pagecount, $paged, $label = "Next", $type = "pl
 /**
  * Detect Single-Paged for split single-page
  */
-function is_single_paged($page){
+function is_single_paged($page) {
 	global $wp_query;
 	if(!is_preview()){
 		$paged = (get_query_var('page')) ? get_query_var('page') : 1;
@@ -313,4 +313,3 @@ function is_single_paged($page){
 		return false;
 	}
 }
-?>
